cfdd64c16375e141c83f1be36bf33d3d190aa87d
Marlon Marques <marlon.belohd@gmail.com>
2022-01-08 17:58:40 -0300

    ensure AlamofireAdapter can call Alamofire with no data

--- InfraTests/AlamorifeAdapterTests.swift
+++ InfraTests/AlamorifeAdapterTests.swift
@@ -8,13 +8,28 @@ class AlamorifeAdapter {
         self.session = session
     }
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
 class AlamorifeAdapterTests: XCTestCase {
-    func test_post_should__make_request_with_valid_url_and_method() {
+    func test_post_should_make_request_with_valid_url_and_method() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamorifeAdapter(session: session)
+        sut.post(to: url, with: nil)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest() { request in
+            XCTAssertNil(request.httpBodyStream)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_post_should__make_request_with_no_data() {
         let url = makeUrl()
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]

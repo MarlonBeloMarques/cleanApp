f1f108839df5c0e466047c3d284f7b4f87dbbc98
Marlon Marques <marlon.belohd@gmail.com>
2022-01-22 10:38:25 -0300

    move AlamofireAdapter to its own file and fix class name AlamofireAdapter

--- InfraTests/AlamorifeAdapterTests.swift
+++ InfraTests/AlamorifeAdapterTests.swift
@@ -1,41 +1,9 @@
 import XCTest
 import Alamofire
+import Infra
 import Data
 
-class AlamorifeAdapter: HttpPostClient {
-    private let session: Session
-    
-    init(session: Session = .default) {
-        self.session = session
-    }
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
-            guard let statusCode = dataResponse.response?.statusCode else { return completion(.failure(.noConnectivity)) }
-            switch dataResponse.result {
-            case .failure: completion(.failure(.noConnectivity))
-            case .success(let data):
-                switch statusCode {
-                case 204:
-                    completion(.success(nil))
-                case 200...299:
-                    completion(.success(data))
-                case 401:
-                    completion(.failure(.unathorized))
-                case 403:
-                    completion(.failure(.forbidden))
-                case 400...499:
-                    completion(.failure(.badRequest))
-                case 500...599:
-                    completion(.failure(.serverError))
-                default:
-                    completion(.failure(.noConnectivity))
-                }
-            }
-        }
-    }
-}
-
-class AlamorifeAdapterTests: XCTestCase {
+class AlamofireAdapterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
         testRequestFor(url: url, data: makeValidData()) { request in
@@ -86,12 +54,12 @@ class AlamorifeAdapterTests: XCTestCase {
     }
 }
 
-extension AlamorifeAdapterTests {
-    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> AlamorifeAdapter {
+extension AlamofireAdapterTests {
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> AlamofireAdapter {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
-        let sut = AlamorifeAdapter(session: session)
+        let sut = AlamofireAdapter(session: session)
         checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }

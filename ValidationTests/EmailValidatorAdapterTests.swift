ee463b8183107db9b0085a70242647953c794aab
Marlon Marques <marlon.belohd@gmail.com>
2022-01-31 08:09:09 -0300

    ensure EmailValidatorAdapter returns true if a valid email is provided

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -21,7 +21,12 @@ class EmailValidatorAdapterTests: XCTestCase {
         XCTAssertFalse(sut.isValid(email: "rr@rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr."))
         XCTAssertFalse(sut.isValid(email: "@rr.com"))
-
-
+    }
+    
+    func test_valid_emails() {
+        let sut = EmailValidatorAdapter()
+        XCTAssertTrue(sut.isValid(email: "marlon@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "marlon.marques@mail.com"))
+        XCTAssertTrue(sut.isValid(email: "marlo@hotmail.com"))
     }
 }

43823655de7bba88b9917614a3ee507cc6ab74ac
Marlon Marques <marlon.belohd@gmail.com>
2022-03-26 11:34:47 -0300

    change Main layer to use functions to create factories

---
+++ Main/Factories/Http/RemoteAddAccountFactory.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Domain
+import Data
+
+func makeRemoteAddAccount(httpClient: HttpPostClient) -> AddAccount {
+    let remoteAddAccount = RemoteAddAccount(url: makeApiUrl(path: "signup"), httpClient: httpClient)
+    return MainQueueDispatchDecorator(remoteAddAccount)
+}

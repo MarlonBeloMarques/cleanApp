812eb2a68e969e7b035106cde5ed566e911d854d
Marlon Marques <marlon.belohd@gmail.com>
2022-03-28 20:25:49 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -9,3 +9,7 @@ func makeAccountModel() -> AccountModel {
 func makeAddAccountModel() -> AddAccountModel {
     return AddAccountModel(name: "any_name", email: "any_email@gmail.com", password: "any_password", passwordConfirmation: "any_password")
 }
+
+func makeAuthenticationModel() -> AuthenticationModel {
+    return AuthenticationModel(email: "any_email@gmail.com", password: "any_password")
+}

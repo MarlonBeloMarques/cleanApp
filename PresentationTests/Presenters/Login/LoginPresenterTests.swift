3dcec871e72361923e91c3371ef5ad2fa40f810e
Marlon Marques <marlon.belohd@gmail.com>
2022-04-01 19:13:11 -0300

    ensure LoginPresenter completes with error if Authentication completes with error

--- PresentationTests/Presenters/Login/LoginPresenterTests.swift
+++ PresentationTests/Presenters/Login/LoginPresenterTests.swift
@@ -31,11 +31,26 @@ class LoginPresenterTests: XCTestCase {
         sut.login(viewModel: makeLoginViewModel())
         XCTAssertEqual(authenticationSpy.authenticationModel, makeAuthenticationModel())
     }
+    
+    func test_login_should_show_generic_error_message_if_authentication_fails() {
+        let alertViewSpy = AlertViewSpy()
+        let authenticationSpy = AuthenticationSpy()
+        let sut = makeSut(alertView: alertViewSpy, authentication: authenticationSpy)
+        let exp = expectation(description: "waiting")
+        alertViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes."))
+            exp.fulfill()
+        }
+        sut.login(viewModel: makeLoginViewModel())
+        authenticationSpy.completeWithError(.unexpected)
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension LoginPresenterTests {
     func makeSut(alertView: AlertViewSpy = AlertViewSpy(), authentication: AuthenticationSpy = AuthenticationSpy(), validation: ValidationSpy = ValidationSpy(), file: StaticString = #filePath, line: UInt = #line) -> LoginPresenter {
         let sut = LoginPresenter(alertView: alertView, authentication: authentication, validation: validation)
+        checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }
 }

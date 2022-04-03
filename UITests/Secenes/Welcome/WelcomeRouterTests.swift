8ae37839a3eda0dada05bb6b51532260ed217ec9
Marlon Marques <marlon.belohd@gmail.com>
2022-04-03 16:34:30 -0300

    move WelcomeRouter to its own file

--- UITests/Secenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Secenes/Welcome/WelcomeRouterTests.swift
@@ -1,26 +1,6 @@
 import XCTest
 import UIKit
-@testable import UI
-
-public final class WelcomeRouter {
-    private let nav: NavigationController
-    private let loginFactory: () -> LoginViewController
-    private let signUpFactory: () -> SignUpViewController
-    
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController) {
-        self.nav = nav
-        self.loginFactory = loginFactory
-        self.signUpFactory = signUpFactory
-    }
-    
-    public func goToLogin() {
-        nav.pushViewController(loginFactory())
-    }
-    
-    public func goToSignUp() {
-        nav.pushViewController(signUpFactory())
-    }
-}
+import UI
 
 class WelcomeRouterTests: XCTestCase {
     func test_goToSignUp_calls_nav_with_correct_vc() {
